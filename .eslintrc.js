36243087268e4d8e6f4d7a0664d2dd8dbbb08520
hbsnow <yuki.4uing@gmail.com>
2020-06-07 12:10:28 +0900

    init

---
+++ .eslintrc.js
@@ -0,0 +1,67 @@
+module.exports = {
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  env: {
+    browser: true,
+    node: true,
+    es2020: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {},
+  overrides: [
+    {
+      files: ["**/*.{ts,tsx}"],
+      extends: [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier/@typescript-eslint",
+      ],
+      parser: "@typescript-eslint/parser",
+      settings: {
+        react: {
+          pragma: "React",
+          version: "detect",
+        },
+      },
+      parserOptions: {
+        sourceType: "module",
+        project: "./tsconfig.json",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      plugins: ["@typescript-eslint", "react", "react-hooks", "import"],
+      rules: {
+        "no-unused-vars": "off",
+        "import/order": [
+          "error",
+          {
+            groups: ["builtin", "external", "internal"],
+            pathGroups: [
+              {
+                pattern: "react",
+                group: "external",
+                position: "before",
+              },
+            ],
+            pathGroupsExcludedImportTypes: ["react"],
+            "newlines-between": "always",
+            alphabetize: {
+              order: "asc",
+              caseInsensitive: true,
+            },
+          },
+        ],
+        "@typescript-eslint/no-unused-vars": "error",
+        "react/prop-types": "off",
+        "react-hooks/rules-of-hooks": "error",
+        "react-hooks/exhaustive-deps": "error",
+      },
+    },
+  ],
+  globals: {
+    React: "writable",
+  },
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { NextPage } from "next";
+
+import DefaultTemplate from "../Templates/DefaultTemplate";
+
+const Page: NextPage = () => {
+  return (
+    <>
+      <DefaultTemplate>sample</DefaultTemplate>
+    </>
+  );
+};
+
+export default Page;
