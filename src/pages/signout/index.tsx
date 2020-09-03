edb71cfc1dca5724cdb1701568769ac1a4d293a5
hbsnow <yuki.4uing@gmail.com>
2020-09-03 09:15:38 +0900

    refactor: signout移動

--- src/pages/signout/index.tsx
+++ src/pages/signout/index.tsx
@@ -3,7 +3,7 @@ import React, { useEffect } from "react";
 import { NextPage } from "next";
 import { useRouter } from "next/dist/client/router";
 
-import { auth } from "../services/firebase/client";
+import { auth } from "../../services/firebase/client";
 
 const Page: NextPage = () => {
   const router = useRouter();
